d5f9774f35f71f6247056f28194afea133862c5d
Samir_yk <152762786+samirc-10@users.noreply.github.com>
2024-12-10 21:22:18 +0545

    Add files via upload

---
+++ greatest_n.py
@@ -0,0 +1,11 @@
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+greatest = max(a, b, c)
+
+print("The greatest number is:", greatest)
+
+
+
